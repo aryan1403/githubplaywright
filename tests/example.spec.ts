7143f9325c15d3aea9b2684ce4e9212a35237706
aryan1403 <aaryan14032006@email.com>
2025-05-02 09:42:52 +0530

    meow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,7 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-
+/*
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -15,4 +15,4 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+});*/
